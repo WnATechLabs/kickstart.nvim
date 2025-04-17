3df66bd97312b4beaea061bb538f5f166226e501
Will <william@wnatechlabs.com>
2025-04-17 12:35:07 -0400

    Adds descriptions to split and tab keymaps
    Adds descriptions to the split and tab keymaps to provide more context when using the keymaps. Also adds keymaps to resize splits using Ctrl + arrow keys.

--- lua/kickstart/config/tabs.lua
+++ lua/kickstart/config/tabs.lua
@@ -1,7 +1,8 @@
 -- Tab management
-vim.keymap.set("n", "<leader>ta", "<cmd>tab split<CR>")
-vim.keymap.set("n", "<leader>tA", "<cmd>tabnew<CR>")
-vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>")
-vim.keymap.set("n", "<leader>to", "<cmd>tabonly<CR>")
-vim.keymap.set("n", "<leader>tn", "<cmd>tabnext<CR>")
-vim.keymap.set("n", "<leader>tp", "<cmd>tabprev<CR>")
+vim.keymap.set("n", "<leader>ta", "<cmd>tab split<CR>", { desc = "[t]ab [a]dd: Split current window into a new tab" })
+vim.keymap.set("n", "<leader>tA", "<cmd>tabnew<CR>", { desc = "[t]ab [A]dd new: Open a new empty tab" })
+vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "[t]ab [c]lose: Close the current tab" })
+vim.keymap.set("n", "<leader>to", "<cmd>tabonly<CR>", { desc = "[t]ab [o]nly: Close all other tabs" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnext<CR>", { desc = "[t]ab [n]ext: Go to the next tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabprev<CR>", { desc = "[t]ab [p]revious: Go to the previous tab" })
+
