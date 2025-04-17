e2985b27fad924746d1f7354fd95cbca7ca6350b
Will <william@wnatechlabs.com>
2025-04-17 12:37:45 -0400

    Adds nvim-surround plugin
    Adds the nvim-surround plugin for managing surrounding characters.
    
    This provides functionality for adding, deleting, and replacing surrounding characters such as brackets and quotes.

--- init.lua
+++ init.lua
@@ -885,13 +885,6 @@ require('lazy').setup({
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
@@ -956,6 +949,7 @@ require('lazy').setup({
   require 'kickstart.plugins.copilot',
   require 'kickstart.plugins.codecompanion',
   require 'kickstart.plugins.vim-dadbod-ui',
+  require 'kickstart.plugins.nvim-surround',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
