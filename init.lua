27898fa4a69dbfc00ba9b16fd20885df5af0623e
Will <william@wnatechlabs.com>
2025-04-17 12:38:58 -0400

    Configures text concealment and code folding
    Adds configuration for text concealment, which enhances
    the user experience, particularly with markdown.
    
    Implements code folding to improve code navigation and
    readability.

--- init.lua
+++ init.lua
@@ -101,6 +101,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Sets the level of text concealment in the editor (e.g., hiding certain syntax elements, useful for markdown)
+vim.opt.conceallevel = 2
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -208,7 +211,7 @@ require('lazy').setup({
         changedelete = { text = '~' },
         untracked    = { text = '┆' },
       },
-      signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
+      signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
       numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
       linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
       word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
@@ -227,7 +230,7 @@ require('lazy').setup({
       current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
       sign_priority                = 6,
       update_debounce              = 100,
-      status_formatter             = nil, -- Use default
+      status_formatter             = nil,   -- Use default
       max_file_length              = 40000, -- Disable if file is longer than this (in lines)
       preview_config               = {
         -- Options passed to nvim_open_win
@@ -997,6 +1000,13 @@ require('lazy').setup({
 
 -- Pick a colorscheme
 vim.cmd.colorscheme "adwaita"
+
+
+-- Code folding
+vim.opt.foldcolumn = '9'
+vim.opt.foldlevel = 20
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
